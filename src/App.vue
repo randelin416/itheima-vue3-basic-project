2477c67d4fcb3519195d21a6bda68474ae3f67c9
randelin416 <konankid10129@gmail.com>
2024-06-13 16:45:05 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,21 @@
 <script setup>
 import Edit from './components/Edit.vue'
+// 載入各個需要的方法
+import { onMounted, ref } from 'vue'
+import axios from'axios'
 
 // TODO: 列表渲染
+// 思路：聲明響應式list -> 調用接口獲取資料 -> 後端數據賦值給list -> 綁定到table組件
 
+const list = ref([])
+const getList = async() => {
+  // 接口調用
+  const res = await axios.get('/list')
+  // 交給list
+  list.value = res.data
+}
 
+onMounted(() => getList())
 // TODO: 删除功能
 
 
@@ -13,11 +25,13 @@ import Edit from './components/Edit.vue'
 
 <template>
   <div class="app">
-    <el-table :data="[{
+    <!-- 靜態資料
+    [{
       id: 1,
       name: 'jack',
       place: 'none'
-    }]">
+    }] -->
+    <el-table :data="list">
       <el-table-column label="ID" prop="id"></el-table-column>
       <el-table-column label="姓名" prop="name" width="150"></el-table-column>
       <el-table-column label="籍贯" prop="place"></el-table-column>
